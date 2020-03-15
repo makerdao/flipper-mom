1217f57e838d81748d65b8d2fd5733ff2d076b99
Kurt Barry <kurt.m.barry@gmail.com>
2020-03-14 17:16:15 -0700

    setOwner and setAuthority tests

--- src/FlipperMom.t.sol
+++ src/FlipperMom.t.sol
@@ -4,18 +4,54 @@ import "ds-test/test.sol";
 
 import "./FlipperMom.sol";
 
+contract MomCaller {
+    FlipperMom mom;
+
+    constructor(FlipperMom mom_) public {
+        mom = mom_;
+    }
+
+    function setOwner(address newOwner) public {
+        mom.setOwner(newOwner);
+    }
+
+    function setAuthority(address newAuthority) public {
+        mom.setAuthority(newAuthority);
+    }
+}
+
 contract FlipperMomTest is DSTest {
     FlipperMom mom;
+    MomCaller caller;
 
     function setUp() public {
         mom = new FlipperMom();
+        caller = new MomCaller(mom);
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function testSetOwner() public {
+        assertTrue(mom.owner() == address(this));
+        assertTrue(mom.owner() != address(0));
+        mom.setOwner(address(0));
+        assertTrue(mom.owner() == address(0));
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    // a contract that does not own the Mom cannot set a new owner
+    function testFailSetOwner() public {
+        assertTrue(mom.owner() != address(caller));
+        caller.setOwner(address(0));
+    }
+
+    function testSetAuthority() public {
+        assertTrue(mom.owner() == address(this));
+        assertTrue(mom.authority() != address(caller));
+        mom.setAuthority(address(caller));
+        assertTrue(mom.authority() == address(caller));
+    }
+
+    // a contract that does not own the Mom cannot set a new authority
+    function testFailSetAuthority() public {
+        assertTrue(mom.owner() != address(caller));
+        caller.setAuthority(address(caller));
     }
 }
